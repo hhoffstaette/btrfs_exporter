ebf8b960a8ed2f102850f1689b7bf63b7575a22a
Holger Hoffstätte <holger@applied-asynchrony.com>
2019-03-25 19:38:33 +0100

    Use better wait method as double-locking is UB

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 
 #include <memory>
 #include <mutex>
+#include <thread>
+
 #include <prometheus/exposer.h>
+
 #include "BtrfsRegistry.h"
 
 int main(int argc, char **argv) {
@@ -9,6 +12,7 @@ int main(int argc, char **argv) {
 	using namespace btrfs_exporter;
 	using namespace prometheus;
 	using namespace std;
+	using namespace std::chrono;
 
 	// create a _metrics registry with component=main labels applied to all its metrics
 	shared_ptr<BtrfsRegistry> registry = make_shared<BtrfsRegistry>();
@@ -20,9 +24,7 @@ int main(int argc, char **argv) {
 	exposer.RegisterCollectable(registry);
 
 	// wait forever (or SIGTERM)
-	mutex m;
-	m.lock();
-	m.lock();
+	this_thread::sleep_until(time_point<system_clock>::max());
 
 	return 0;
 }
